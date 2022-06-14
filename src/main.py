abbfbf1dda40155f8209570fbe0f013cc80730d2
CyrilSmed <cyrilsmed@outlook.com>
2022-06-14 21:18:26 +0500

    Neural network submission

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 import pandas as pd
+import numpy as np
 
 import data_analysis
 import data_processing
 import decision_tree
 import model_selection
 
-def predict_and_save(model, ids, X_test) -> None:
+from sklearn.model_selection import train_test_split
+
+def predict_and_save(model, ids, X_test, file_name, is_neural_net=False) -> None:
     pred_df = pd.DataFrame(ids)
-    y_pred = decision_tree.predict(X_test)
+    y_pred = model.predict(X_test)
+
+    if is_neural_net:
+        y_pred = (y_pred > 0.5).astype("int32")
+        y_pred = list(map(lambda x: x[0], y_pred))
+
     pred_df["Survived"] = pd.Series(y_pred)
-    pred_df.to_csv(r"../datasets/decision_tree_submission.csv", index=False)
+    pred_df.to_csv(r"../datasets/{0}.csv".format(file_name), index=False)
 
 def get_score(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
@@ -20,10 +28,17 @@ if __name__ == "__main__":
     pd.set_option("display.max_rows", None, "display.max_columns", None)
 
     df = data_processing.clean_training_dataset(df)
-    # decision_tree = decision_tree.get_trained_model(df)
 
-    # test_df = pd.read_csv(r"../datasets/test.csv", sep=",", header="infer", names=None, encoding="utf-8")
-    # ids, test_df = data_cleaning.normalize_for_prediction(test_df)
-    # predict_and_save(decision_tree, ids, test_df)
+    # model_selection.compare(df)
+    target_col = "Survived"
+    data = df.loc[:, df.columns != target_col]
+    norm_data = (data - data.min()) / (data.max() - data.min())
+    target = df.loc[:, target_col]
+    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3)
 
-    model_selection.compare(df)
+    model = model_selection.build_neural_net()
+    model.fit(X_train, y_train, epochs=1000)
+
+    test_df = pd.read_csv(r"../datasets/test.csv", sep=",", header="infer", names=None, encoding="utf-8")
+    ids, test_df = data_processing.normalize_for_prediction(test_df)
+    predict_and_save(model, ids, test_df, file_name="neural_net_submission", is_neural_net=True)
--- src/model_selection.py
+++ src/model_selection.py
@@ -1,13 +1,29 @@
 import pandas as pd
 import tensorflow as tf
+from keras.wrappers.scikit_learn import KerasClassifier
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import matthews_corrcoef
 from sklearn.tree import DecisionTreeClassifier
 from statistics import mean
+
+import data_analysis
+
+def build_neural_net(optimizer="rmsprop", dense_dims=32):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Input(shape=8))
+    model.add(tf.keras.layers.Dense(units=10, activation="sigmoid"))
+    model.add(tf.keras.layers.Dense(units=10, activation="relu"))
+    model.add(tf.keras.layers.Dense(units=1, activation="sigmoid"))
+    model.compile(optimizer=optimizer,
+                  loss="binary_crossentropy",
+                  metrics=["accuracy"])
+    return model
 
 def compare(df: pd.DataFrame):
     log_reg_scores = []
@@ -19,14 +35,19 @@ def compare(df: pd.DataFrame):
     norm_data = (data - data.min()) / (data.max() - data.min())
     target = df.loc[:, target_col]
 
-    print_cross_val_score(LogisticRegression(max_iter=300), data, target)
+    model = KerasClassifier(build_neural_net, epochs=1000)
+    X_train, X_test, y_train, y_test = train_test_split(norm_data, target, test_size=0.3)
+    model.fit(X_train, y_train)
+    data_analysis.analize_model(model, X_test=X_test, y_test=y_test)
 
-    print_cross_val_score(RandomForestClassifier(
-        n_estimators=100, criterion="entropy",
-        max_depth=5, random_state=1), data, target)
-
-    print_cross_val_score(DecisionTreeClassifier(
-        criterion="entropy", max_depth=5), data, target)
+    # print_cross_val_score(LogisticRegression(max_iter=300), data, target)
+    #
+    # print_cross_val_score(RandomForestClassifier(
+    #     n_estimators=100, criterion="entropy",
+    #     max_depth=5, random_state=1), data, target)
+    #
+    # print_cross_val_score(DecisionTreeClassifier(
+    #     criterion="entropy", max_depth=5), data, target)
 
 def print_cross_val_score(model, data, target):
     scores = cross_val_score(model, data, target)
